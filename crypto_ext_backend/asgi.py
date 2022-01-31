4dd7d9ddc902618031a175916fca0ffe5d447d57
mdShakilHossainNsu2018 <shakilnsu2018@gmail.com>
2022-02-01 00:57:50 +0600

    Initial commit

---
+++ crypto_ext_backend/asgi.py
@@ -0,0 +1,26 @@
+"""
+ASGI config for crypto_ext_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
+from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crypto_ext_backend.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
