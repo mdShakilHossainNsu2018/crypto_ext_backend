879c6a257c52afec670756fe4681502c26cf6aaf
mdShakilHossainNsu2018 <shakilnsu2018@gmail.com>
2022-03-18 09:37:04 +0600

    asyc

--- core/consumers.py
+++ core/consumers.py
@@ -1,7 +1,7 @@
 import json
 from asgiref.sync import async_to_sync
 from celery import shared_task
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
 import requests
 from crypto_ext_backend.settings import BTC_GROUP, CRYPTO_GROUP, CRYPTO_ROOM, CRYPTO_ALARM_ROOM, CRYPTO_ALARM_GROUP
@@ -79,34 +79,34 @@ def send_crypto_message():
     )
 
 
-class BTCConsumer(WebsocketConsumer):
-    def connect(self):
+class BTCConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         # self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_name = BTC_GROUP
         self.room_group_name = 'group_%s' % self.room_name
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -115,81 +115,81 @@ class BTCConsumer(WebsocketConsumer):
         )
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
 
 
-class AllCryptoConsumer(WebsocketConsumer):
+class AllCryptoConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.room_group_name = CRYPTO_GROUP
         self.room_name = CRYPTO_ROOM
 
-    def connect(self):
+    async def connect(self):
         # self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_name = CRYPTO_ROOM
         self.room_group_name = CRYPTO_GROUP
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
 
 
-class CryptoAlarmConsumer(WebsocketConsumer):
+class CryptoAlarmConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.room_group_name = CRYPTO_ALARM_GROUP
         self.room_name = CRYPTO_ALARM_ROOM
 
-    def connect(self):
+    async def connect(self):
         # self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_name = CRYPTO_ALARM_ROOM
         self.room_group_name = CRYPTO_ALARM_GROUP
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
